a042da527c05df9b959f94b3bb327851503cabd3
Haochen Li <haochenl@usc.edu>
2017-05-16 11:10:19 -0700

    Added samtools sort and index functions

--- samtools.py
+++ samtools.py
@@ -10,6 +10,7 @@ import os
 import utils
 import subprocess
 
+
 def run_samtools_concatenation(bam_file_list, combined_bam_filename):
     """
     Concatenate a list of bam file into a single bam file without disturbing the orignial order
@@ -19,4 +20,24 @@ def run_samtools_concatenation(bam_file_list, combined_bam_filename):
     args = ["samtools", "cat", "-o", combined_bam_filename]
     for filename in bam_file_list:
         args.append(filename)
+    subprocess.call(args)
+
+
+def run_samtools_sort(input_bam_filename, output_bam_prefix, threads):
+    """
+    Sort bam file by position
+    :param input_bam_filename: input bam file name.
+    :param output_bam_prefix: output bam file prefix
+    :param threads: number of threads for sorting
+    """
+    args = ["samtools", "sort", "-@", str(threads), input_bam_filename, "-o", output_bam_prefix]
+    subprocess.call(args)
+
+
+def run_samtools_index(sorted_bam_filename):
+    """
+    Index the sorted bam file
+    :param sorted_bam_filename: input bam file to index.
+    """
+    args = ["samtools", "index", sorted_bam_filename]
     subprocess.call(args)
